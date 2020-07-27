c342eaa5ec75bb333619f3bc7c469eaebbd6131a
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-07-27 14:02:05 -0500

    Update dyld_shared_cache_util formula

--- dyld_shared_cache_util.rb
+++ dyld_shared_cache_util.rb
@@ -1,7 +1,7 @@
 class DyldSharedCacheUtil < Formula
     desc "Apple's utility for many dyld cache operations built for the general public"
     homepage "https://opensource.apple.com/source/dyld/dyld-733.8/"
-    url "https://github.com/DiegoMagdaleno/dyld.git", :tag => "733.8-2"
+    url "https://github.com/DiegoMagdaleno/dyld.git", :tag => "733.8-3"
 
 
     depends_on :macos => :catalina # TODO: Once homebrew supports Big Sur make it Big Sur+
