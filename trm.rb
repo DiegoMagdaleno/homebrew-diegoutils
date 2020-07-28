7c5d946093af4a419dec47c5d3bc294f6cebe7bd
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-07-28 12:16:53 -0500

    Update trm formulae

--- trm.rb
+++ trm.rb
@@ -1,7 +1,7 @@
 class Trm < Formula
     desc "Small utility to move files to the trash can instead of directly deleting them. Written in Go."
     homepage "https://github.com/DiegoMagdaleno/trm"
-    url "https://github.com/DiegoMagdaleno/trm.git", :tag => "v0.0.5"
+    url "https://github.com/DiegoMagdaleno/trm.git", :tag => "v0.0.6"
 
     depends_on "go" => :build
 
