8ca8d56ad9b916825cfe086c21bc253832b25a04
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-07-28 11:46:50 -0500

    Add trm (New Formula)

---
+++ trm.rb
@@ -0,0 +1,18 @@
+class Trm < Formula
+    desc "Small utility to move files to the trash can instead of directly deleting them. Written in Go."
+    homepage "https://github.com/DiegoMagdaleno/trm"
+    url "https://github.com/DiegoMagdaleno/trm.git", :tag => "v0.0.1"
+
+    depends_on "go" => build
+
+    def install
+        system "make", "install", "PREFIX=#{prefix}"
+    end
+
+    test do
+        (testpath/"test.file").write <<~EOS
+            This is a file
+        EOS
+        system "#{bin}/trm", "test.file"
+    end
+end
