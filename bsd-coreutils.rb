00feadb257dbbe4e6a6d1a7a8058835af1216b75
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-01 12:40:28 -0500

    Try a new method for OpenSSL vars

--- bsd-coreutils.rb
+++ bsd-coreutils.rb
@@ -1,4 +1,4 @@
-class BsdCoreutils < Formula
+class BsdCoreUtils < Formula
     desc "The OpenBSD Coreutils Project is a port of many utilities from OpenBSD to Linux and macOS."
     homepage "https://github.com/DiegoMagdaleno/BSDCoreUtils"
     url "https://github.com/DiegoMagdaleno/BSDCoreUtils.git", :tag => "v6.7-r1"
@@ -6,12 +6,13 @@ class BsdCoreutils < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "openssl@1.1"
-    depends_on "pkg-config"
 
     def install
+        ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl@1.1"].opt_lib}/pkgconfig"
+        ENV.prepend_path "LDFLAGS", "#{Formula["openssl@1.1"].opt_lib}"
+        ENV.prepend_path "CPPFLAGS", "#{Formula["openssl@1.1"].opt_include}"
         system "./autogen.sh"
-        system "PKG_CONFIG_PATH=\"#{Formula["openssl"].opt_prefix/lib/pkgconfig}\"", "CPPFLAGS=\"-I#{Formula["openssl"].opt_prefix/include}\"", "LDFLAGS=\"-L#{Formula["openssl"].opt_prefix/lib}\"","./configure", "--prefix=#{prefix}", "--program-prefix=b"
+        system "./configure", "--prefix=#{prefix}", "--program-prefix=b"
         system "make"
         system "make install"
     end
