abac7d0d64b11de5ed3706f5926e46a3cceb9921
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-10-20 19:58:13 -0500

    Add duckx.rb formulae

---
+++ duckx.rb
@@ -0,0 +1,17 @@
+class Duckx < Formula
+    desc "Create, read and write Microsoft Office Word docx files."
+    homepage "https://github.com/amiremohamadi/DuckX"
+    head "https://github.com/amiremohamadi/DuckX.git"
+    license "MIT"
+
+    depends_on "cmake" => :build
+    depends_on "pugixml"
+
+    def install
+        mkdir "build" do
+            system "cmake", "..", *std_cmake_args
+            system "cmake", "--build", "."
+            system "make", "install"
+        end
+    end
+end
