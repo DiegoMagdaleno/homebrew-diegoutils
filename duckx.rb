12bfff1b20fa96e4ae88fdc489b4a11a427edaee
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-10-20 21:13:16 -0500

    Temporarily use my fork
    - My fork fixes the issues with clang,
    this will be what I use until the patch
    is merged

--- duckx.rb
+++ duckx.rb
@@ -1,16 +1,12 @@
 class Duckx < Formula
     desc "Create, read and write Microsoft Office Word docx files."
     homepage "https://github.com/amiremohamadi/DuckX"
-    head "https://github.com/amiremohamadi/DuckX.git"
+    head "https://github.com/DiegoMagdaleno/DuckX"
     license "MIT"
 
     depends_on "cmake" => :build
     depends_on "pugixml"
     depends_on "kuba-zip"
-
-    depends_on "gcc"
-
-    fails_with :clang
 
     def install
         mkdir "build" do
