8fcc020ed80ec5ac6528a6a520563c25363e1bbf
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-11-01 22:08:40 -0600

    Add libtags

---
+++ libtags.rb
@@ -0,0 +1,15 @@
+class LibTags < Formula
+    desc "Obj-C library for manipulating the tags on Files"
+    homepage "https://github.com/DiegoMagdaleno/libtags"
+    head "https://github.com/DiegoMagdaleno/libtags.git"
+    license "MIT"
+
+    def install
+        xcodebuild "-project", "libtags.xcodeproj",
+        "-scheme", "libtags",
+        "SYMROOT=build",
+        "DSTROOT=#{prefix}",
+        "-verbose",
+        "install"
+    end
+end 
